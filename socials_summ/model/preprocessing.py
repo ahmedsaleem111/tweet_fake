5a9ad8f57c92ded62dd27585107228c89a0d7871
Ahmed Saleem <ahmed.saleem1111@gmail.com>
2022-12-09 16:30:57 -0800

    Mods 12/9/2022 4:30 PM - AS

--- socials_summ/model/preprocessing.py
+++ socials_summ/model/preprocessing.py
@@ -3,6 +3,52 @@ import datetime
 
 
 
+
+
+class Source():
+    valid_types = ["Image", "Video"]
+    def __init__(self, url, type='Image'):
+        self.__url = url        
+        self.__type = type 
+
+    @property
+    def url(self): return self.__url
+    @property
+    def type(self): return self.__type
+
+
+
+
+class Post():
+    valid_types = ["Instagram", "Twitter", "Facebook"]
+    def __init__(self,
+        timestamp,
+        type="Instagram",
+        sources=None, # list of Source Objects
+        caption=None,
+        location=None    
+    ):
+        self.__timestamp =timestamp
+        self.__type = type
+        self.__sources = sources
+        self.__caption = caption
+        self.__location = location
+
+    @property
+    def type(self): return self.__type
+    @property
+    def timestamp(self): return self.__timestamp
+    @property
+    def sources(self): return self.__sources
+    @property
+    def caption(self): return self.__caption
+    @property
+    def location(self): return self.__location
+
+
+
+
+
 '''
 Specify a dummy root user (to allow access to public accounts)
 
@@ -47,11 +93,38 @@ class InstagramPreProcessor(PreProcessor):
         if self.accountType=="public":
             if len(posts)==0: raise Exception('Specifed account is private. Please provide password!')
             else: 
-                posts = [post.__dict__ for post in posts]
-                
-                return posts
-        else: # private account
-            return posts # how ?
+                posts_pre_processed = [] # pre-processed into list of "Post" objects
+
+                for post in posts:
+                    dict_ = post.__dict__
+
+
+
+
+                    timestamp = dict_["taken_at"]
+                    caption = dict_["caption_text"]
+
+                    loc = dict_["location"]
+                    if loc is None: location = None
+                    else: location = [loc.__dict__["name"], loc.__dict__["city"]]
+
+
+                    # How to get media sources? (or simply "sources")
+                    posts_pre_processed.append(
+                        Post(
+                            timestamp,
+                            type="Instagram",
+                            caption=caption,
+                            location=location
+                        )
+                    )
+
+                return posts_pre_processed
+
+        else: # private mode
+            raise NotImplementedError('"Private"-mode not yet implemented!')
+
+
 
 class TwitterPreProcessor(PreProcessor):
 
@@ -83,8 +156,8 @@ if __name__ == "__main__":
     for post in posts: 
         
         print('\n\n\n')
-        for key, val in post.items():
-            print(key, val)
+        for key, value in post.__dict__.items():
+            print(key, value)
 
 
 
