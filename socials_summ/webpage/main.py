15abab8d45cea66649aa5020969d715326da5b8a
Josip <26150641+JosipAndrasec@users.noreply.github.com>
2022-12-10 01:07:22 +0000

    added additional input fields

--- socials_summ/webpage/main.py
+++ socials_summ/webpage/main.py
@@ -1,19 +1,21 @@
 import gradio as gr
 
-def social_summary(username, year):
+def social_summary(username_ig, password_ig, username_twitter, password_twitter, year):
     # model()
     return "output here..."
 
 with gr.Blocks() as ui:
     with gr.Row():
         with gr.Column():
-            username = gr.Textbox(label="Twitter username:")
+            username_ig = gr.Textbox(label="Instagram username:")
+            password_ig = gr.Textbox(label="Instagram password:")
+            username_twitter = gr.Textbox(label="Twitter username:")
+            password_twitter = gr.Textbox(label="Twitter password:")
             year = gr.Textbox(label="Specify year:")
             button = gr.Button(value="Wrap the Year")
         with gr.Column():
             summary = gr.Textbox(label="Summary")
 
-    button.click(social_summary, inputs=[username, year], outputs=summary)
-    examples = gr.Examples(examples=[["test1", "2022"], ["test2", "2021"]], inputs=[username, year])
+    button.click(social_summary, inputs=[username_ig, password_ig, username_twitter, password_twitter, year], outputs=summary)
 
 ui.launch()
