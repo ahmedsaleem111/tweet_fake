fd6dff7058b0c0d7420a9d43ff25091bcfce8a6a
Ahmed Saleem <ahmed.saleem1111@gmail.com>
2022-12-11 11:35:56 -0800

    final ui mod

--- website/views.py
+++ website/views.py
@@ -20,39 +20,9 @@ def landing():
         depth2Var=False,
         # summary_text=''
         tweets = tweets,
-        background_image=os.path.join('static', 'fake_image.png')
+        logo_image=os.path.join('static', 'logo_image.jpg'),
     )
 
-
-# @views.route('/landing', methods=('GET', 'POST'))
-# def submit_credentials():
-#     if request.method == 'POST':
-#
-#         # ig_handle = request.form.get('ig_handle')
-#         # tw_handle = request.form.get('tw_handle'),
-#         # fb_handle = request.form.get('fb_handle'),
-#
-#         # ipp = InstagramPreProcessor(ig_handle)
-#         # posts = ipp.getPosts()
-#         #
-#         # for post in posts:
-#         #
-#         #     print('\n\n\n')
-#         #     for key, value in post.__dict__.items():
-#         #         print(key, value)
-#         #
-#         # corpus = Corpus(*posts)
-#
-#         # ---- call server to run model --- get summary
-#
-#         summary_text = 'In 2022, I graduated from CSUN with BSc in EE, \
-#             went to the Bahamas in the Summer, and finished the year off \
-#             getting engaged in Dubai!!'
-#
-#     return render_template("landing.html",
-#         depth2Var=True,
-#         summary_text=summary_text
-#     )
 
 @views.route('/tweet', methods=['POST'])
 def submit_credentials():
